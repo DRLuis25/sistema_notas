f1b79ac2c84410e04b3c4b3d1dd39cc0de9a4c5c
DRLuis25 <ldelgado@unitru.edu.pe>
2021-01-29 20:32:24 -0500

    Init project

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'retrieved' => ':model recuperado.',
+    'saved'     => ':model guardado.',
+    'updated'   => ':model actualizado.',
+    'deleted'   => ':model eliminado.',
+    'not_found' => ':model no encontrado',
+
+];
